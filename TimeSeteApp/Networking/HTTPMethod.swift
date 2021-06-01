a4c9cde0b55735e39e8c03adfa1072b4823fb325
Mayara <mmayolliveira@gmail.com>
2021-06-01 12:33:03 -0400

    definição dos métodos HTTP

---
+++ TimeSeteApp/Networking/HTTPMethod.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPMethod.swift
+//  TimeSeteApp
+//
+//  Created by Mayara Ferreira de Oliveira on 01/06/21.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    
+    case get = "GET"
+    case post = "POST"
+}
